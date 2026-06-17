0c748aa38acec7ee83ebb4a056701ce55135a3cd
Mikael Santos <mykaelsantos@outlook.com.br>
2026-06-17 17:21:38 -0300

    add register with fluentvalidation

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
@@ -0,0 +1,13 @@
+﻿using MyRecipeBook.Communication.Requets;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserAccountUseCase
+{
+    public void Execute(RequestRegisterUserAccountJson request)
+    {
+        var validation = new RegisterUserAccountValidation();
+
+        var result = validation.Validate(request);
+    }
+}
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidation.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requets;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserAccountValidation : AbstractValidator<RequestRegisterUserAccountJson>
+{
+    public RegisterUserAccountValidation()
+    {
+        RuleFor(user => user.Name).NotEmpty().WithMessage("Name is required.");
+        RuleFor(user => user.Email).NotEmpty().WithMessage("Email is required.");
+        RuleFor(user => user.Password).NotEmpty().WithMessage("Password is required.");
+        When(user => string.IsNullOrEmpty(user.Email), () =>
+        {
+            RuleFor(user => user.Email).EmailAddress().WithMessage("Invalid email format.");
+        });
+    }
+}
