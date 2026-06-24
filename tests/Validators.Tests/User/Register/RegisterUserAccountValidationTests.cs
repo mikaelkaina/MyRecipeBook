21c4b0f337b6f6f7a7c4038cc5389670848d682d
Mikael Santos <mykaelsantos@outlook.com.br>
2026-06-24 18:04:36 -0300

    validation tests

---
+++ tests/Validators.Tests/User/Register/RegisterUserAccountValidationTests.cs
@@ -0,0 +1,24 @@
+﻿using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Communication.Requets;
+
+namespace Validators.Tests.User.Register;
+
+public class RegisterUserAccountValidationTests
+{
+    [Fact]
+    public void Sucess()
+    {
+        var request = new RequestRegisterUserAccountJson
+        {
+            Name = "John Doe",
+            Email = "john.doe@example.com",
+            Password = "Password123!"
+        };
+
+        var validator = new RegisterUserAccountValidation();
+
+        var result = validator.Validate(request);
+
+        Assert.True(result.IsValid);
+    }
+}
