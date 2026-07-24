d5e87a29b1468476c776f00707c8423011eac0cf
Mikael Santos <mykaelsantos@outlook.com.br>
2026-07-24 07:40:43 -0300

    migrations recipes

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAcess/Configurations/RecipeDishTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Infrastructure.DataAcess.Configurations;
+
+internal class RecipeDishTypeConfiguration : IEntityTypeConfiguration<RecipeDishType>
+{
+    public void Configure(EntityTypeBuilder<RecipeDishType> builder)
+    {
+        builder.ToTable("RecipeDishTypes");
+
+        builder.HasKey(dt => dt.Id);
+
+        builder.Property(dt => dt.Active)
+            .IsRequired()
+            .HasDefaultValue(true);
+
+        builder.Property(dt => dt.Type)
+            .IsRequired()
+            .HasConversion<int>();
+
+        builder.HasOne<Recipe>()
+            .WithMany(r => r.DishTypes)
+            .HasForeignKey(dt => dt.RecipeId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAcess/Configurations/RecipeIngredientConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Infrastructure.DataAcess.Configurations;
+
+internal class RecipeIngredientConfiguration : IEntityTypeConfiguration<RecipeIngredient>
+{
+    public void Configure(EntityTypeBuilder<RecipeIngredient> builder)
+    {
+        builder.ToTable("RecipeIngredients");
+
+        builder.HasKey(ri => ri.Id);
+
+        builder.Property(ri => ri.Active)
+            .IsRequired()
+            .HasDefaultValue(true);
+
+        builder.Property(ri => ri.Item)
+            .IsRequired()
+            .HasMaxLength(250);
+
+        builder.HasOne<Recipe>()
+            .WithMany(r => r.Ingredients)
+            .HasForeignKey(ri => ri.RecipeId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
