317597dc03f47635974572c879b7bd2e1ac82137
Mikael Santos <mykaelsantos@outlook.com.br>
2026-07-24 09:06:49 -0300

    request recipes

---
+++ src/Shared/MyRecipeBook.Communication/Requets/RequestRecipeJson.cs
@@ -0,0 +1,12 @@
+﻿using MyRecipeBook.Communication.Enums;
+
+namespace MyRecipeBook.Communication.Requets;
+
+public class RequestRecipeJson
+{
+    public string Title { get; set; } = string.Empty;
+    public CookTime CookTime { get; set; }
+    public IList<string> Ingredients { get; set; } = [];
+    public ICollection<RequestRecipeInstructionJson> Instructions { get; set; } = [];
+    public ICollection<DishType> DishTypes { get; set; } = [];
+}
