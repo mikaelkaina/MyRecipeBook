cd4470b47a2b82a45dc33e25b4f5f04eeeed8fa7
Mikael Santos <mykaelsantos@outlook.com.br>
2026-07-31 09:06:42 -0300

    add tests integration recipe register

---
+++ tests/WebApi.Tests/Recipe/RecipeRegisterTests.cs
@@ -0,0 +1,99 @@
+﻿using CommonTestsUtilities.Entities;
+using CommonTestsUtilities.Requests;
+using CommonTestsUtilities.Secutiry;
+using MyRecipeBook.Exception;
+using Shouldly;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using WebApi.Tests.InLineData;
+using WebApi.Tests.Resource;
+
+namespace WebApi.Tests.Recipe;
+
+public class RecipeRegisterTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private const string REQUEST_URI = "/api/recipes";
+    private readonly HttpClient _httpClient;
+    private readonly CustomWebApplicationFactory _factory;
+
+    public RecipeRegisterTests(CustomWebApplicationFactory factory)
+    {
+        _factory = factory;
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var dbContext = await _factory.GetDbContext();
+        await dbContext.Users.AddAsync(user);
+        await dbContext.SaveChangesAsync();
+
+        var token = JwtTokenBuilder.Build(
+            user,
+            _factory.GetJwtSigningKey(),
+            _factory.GetJwtExpirationTimeMinutes());
+
+        _httpClient.DefaultRequestHeaders.Authorization =
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        var request = RequestRecipeJsonBuilder.Build();
+
+        var response = await _httpClient.PostAsJsonAsync(REQUEST_URI, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.GetProperty("id").GetString().ShouldNotBeNullOrEmpty();
+        responseData.RootElement.GetProperty("title").GetString().ShouldBe(request.Title);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInLineData))]
+    public async Task Validate_ShouldBeAnErrorResponse_WhenTitleIsEmpty(string culture)
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var dbContext = await _factory.GetDbContext();
+        await dbContext.Users.AddAsync(user);
+        await dbContext.SaveChangesAsync();
+
+        var token = JwtTokenBuilder.Build(
+            user,
+            _factory.GetJwtSigningKey(),
+            _factory.GetJwtExpirationTimeMinutes());
+
+        _httpClient.DefaultRequestHeaders.Authorization =
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Clear();
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.ParseAdd(culture);
+
+        var request = RequestRecipeJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        var response = await _httpClient.PostAsJsonAsync(REQUEST_URI, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedErrorMessage = ResourceMessagesException.ResourceManager
+            .GetString("VALIDATION_TITLE_REQUIRED", new CultureInfo(culture));
+
+        errors.ShouldSatisfyAllConditions(errorsList =>
+        {
+            errorsList.Count().ShouldBe(1);
+            errorsList.ShouldContain(error => error.GetString()!.Equals(expectedErrorMessage));
+        });
+    }
+}
