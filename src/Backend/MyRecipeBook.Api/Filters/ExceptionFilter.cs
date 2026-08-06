a26f808392779aeec8a38142a94ca6450250afe8
Mikael Santos <mykaelsantos@outlook.com.br>
2026-08-06 16:00:04 -0300

    refactoring execptionfilter

--- src/Backend/MyRecipeBook.Api/Filters/ExceptionFilter.cs
+++ src/Backend/MyRecipeBook.Api/Filters/ExceptionFilter.cs
@@ -8,18 +8,25 @@ namespace MyRecipeBook.Api.Filters;
 
 public class ExceptionFilter : IExceptionFilter
 {
+    private readonly ILogger<ExceptionFilter> _logger;
+
+    public ExceptionFilter(ILogger<ExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
+
     public void OnException(ExceptionContext context)
     {
         if (context.Exception is MyRecipeBookException myRecipeBookException)
         {
             context.HttpContext.Response.StatusCode = (int)myRecipeBookException.GetStatusCode();
-
             context.Result = new ObjectResult(new ResponseErrorJson(myRecipeBookException.GetErrorMessages()));
         }
         else
         {
-            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            _logger.LogError(context.Exception, "Unhandled exception");
 
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
             context.Result = new ObjectResult(new ResponseErrorJson(ResourceMessagesException.UNKNOWN_ERROR));
         }
     }
