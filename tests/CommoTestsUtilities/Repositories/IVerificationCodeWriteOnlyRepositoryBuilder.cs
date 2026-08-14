7691927f0af8616d983a42cf0ec5da36fb1f28e8
Mikael Santos <mykaelsantos@outlook.com.br>
2026-08-14 00:16:36 -0300

    add tests use case request password recovery

---
+++ tests/CommoTestsUtilities/Repositories/IVerificationCodeWriteOnlyRepositoryBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Moq;
+using MyRecipeBook.Domain.Repositories.VerificationCode;
+
+namespace CommonTestsUtilities.Repositories;
+
+public class IVerificationCodeWriteOnlyRepositoryBuilder
+{
+    public static IVerificationCodeWriteOnlyRepository Build()
+    {
+        var mock = new Mock<IVerificationCodeWriteOnlyRepository>();
+        return mock.Object;
+    }
+}
