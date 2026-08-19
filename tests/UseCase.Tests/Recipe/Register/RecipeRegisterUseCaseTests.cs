149451733347bcbfc2fd55f80e4525ae0b77d9e0
Mikael Santos <mykaelsantos@outlook.com.br>
2026-08-18 21:57:20 -0300

    refactoring tests use case recipe register with file builder

--- tests/UseCase.Tests/Recipe/Register/RecipeRegisterUseCaseTests.cs
+++ tests/UseCase.Tests/Recipe/Register/RecipeRegisterUseCaseTests.cs
@@ -1,4 +1,5 @@
 ﻿using CommonTestsUtilities.Entities;
+using CommonTestsUtilities.Files;
 using CommonTestsUtilities.Identity;
 using CommonTestsUtilities.Repositories;
 using CommonTestsUtilities.Requests;
@@ -7,6 +8,7 @@ using MyRecipeBook.Application.UseCases.Recipe.Register;
 using MyRecipeBook.Exception;
 using MyRecipeBook.Exception.ExceptionsBase;
 using Shouldly;
+using System.Net;
 
 namespace UseCase.Tests.Recipe.Register;
 
@@ -18,7 +20,7 @@ public class RecipeRegisterUseCaseTests
     }
 
     [Fact]
-    public async Task Success()
+    public async Task Success_WithoutImage()
     {
         var (user, _) = UserBuilder.Build();
 
@@ -26,11 +28,58 @@ public class RecipeRegisterUseCaseTests
 
         var useCase = CreateUseCase(user);
 
-        var result = await useCase.Execute(request);
+        var result = await useCase.Execute(request, recipeIllustration: null);
 
         result.ShouldNotBeNull();
         result.Id.ShouldNotBe(Guid.Empty);
         result.Title.ShouldBe(request.Title);
+    }
+
+    [Fact]
+    public async Task Success_WhenImageIsPng()
+    {
+        var (user, _) = UserBuilder.Build();
+        var request = RequestRecipeJsonBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        var result = await useCase.Execute(request, recipeIllustration: FileBuilder.GetPng());
+
+        result.ShouldNotBeNull();
+        result.Title.ShouldBe(request.Title);
+    }
+
+    [Fact]
+    public async Task Success_WhenImageIsJpeg()
+    {
+        var (user, _) = UserBuilder.Build();
+        var request = RequestRecipeJsonBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        var result = await useCase.Execute(request, recipeIllustration: FileBuilder.GetJpeg());
+
+        result.ShouldNotBeNull();
+        result.Title.ShouldBe(request.Title);
+    }
+
+    [Fact]
+    public async Task Error_WhenImageIsTxt()
+    {
+        var (user, _) = UserBuilder.Build();
+        var request = RequestRecipeJsonBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        var exception = await useCase.Execute(request, recipeIllustration: FileBuilder.GetTxt()).ShouldThrowAsync<ErrorOnValidationException>();
+
+        exception.GetStatusCode().ShouldBe(HttpStatusCode.BadRequest);
+
+        exception.GetErrorMessages().ShouldSatisfyAllConditions(errorMessages =>
+        {
+            errorMessages.Count.ShouldBe(1);
+            errorMessages.ShouldContain(ResourceMessagesException.VALIDATON_ONLY_IMAGES_ACCEPTED);
+        });
     }
 
     [Fact]
