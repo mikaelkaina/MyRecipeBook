40478ebca086ac04693354ff4cdf4d674aa06e61
Mikael Santos <mykaelsantos@outlook.com.br>
2026-08-19 14:25:33 -0300

    test inegration change profile picture

---
+++ tests/WebApi.Tests/User/ChangeProfilePicture/ChangeProfilePictureTests.cs
@@ -0,0 +1,121 @@
+﻿using CommonTestsUtilities.Entities;
+using CommonTestsUtilities.Files;
+using CommonTestsUtilities.Secutiry;
+using MyRecipeBook.Exception;
+using Shouldly;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using WebApi.Tests.InLineData;
+using WebApi.Tests.Resource;
+
+namespace WebApi.Tests.User.ChangeProfilePicture;
+
+public class ChangeProfilePictureTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private const string REQUEST_URI = "/api/users/profile-picture";
+    private readonly HttpClient _httpClient;
+    private readonly CustomWebApplicationFactory _factory;
+
+    public ChangeProfilePictureTests(CustomWebApplicationFactory factory)
+    {
+        _factory = factory;
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Success_WhenPng()
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var dbContext = await _factory.GetDbContext();
+        await dbContext.Users.AddAsync(user);
+        await dbContext.SaveChangesAsync();
+
+        var token = JwtTokenBuilder.Build(
+            user,
+            _factory.GetJwtSigningKey(),
+            _factory.GetJwtExpirationTimeMinutes());
+
+        _httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        var response = await _httpClient.PutAsync(REQUEST_URI, BuildFormData(FileBuilder.GetPng(), "profilePicture"));
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task Success_WhenJpeg()
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var dbContext = await _factory.GetDbContext();
+        await dbContext.Users.AddAsync(user);
+        await dbContext.SaveChangesAsync();
+
+        var token = JwtTokenBuilder.Build(
+            user,
+            _factory.GetJwtSigningKey(),
+            _factory.GetJwtExpirationTimeMinutes());
+
+        _httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        var response = await _httpClient.PutAsync(REQUEST_URI, BuildFormData(FileBuilder.GetJpeg(), "profilePicture"));
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInLineData))]
+    public async Task Error_WhenImageIsTxt(string culture)
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var dbContext = await _factory.GetDbContext();
+        await dbContext.Users.AddAsync(user);
+        await dbContext.SaveChangesAsync();
+
+        var token = JwtTokenBuilder.Build(
+            user,
+            _factory.GetJwtSigningKey(),
+            _factory.GetJwtExpirationTimeMinutes());
+
+        _httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Clear();
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.ParseAdd(culture);
+
+        var response = await _httpClient.PutAsync(REQUEST_URI, BuildFormData(FileBuilder.GetTxt(), "profilePicture"));
+
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedErrorMessage = ResourceMessagesException.ResourceManager
+            .GetString("VALIDATON_ONLY_IMAGES_ACCEPTED", new CultureInfo(culture));
+
+        errors.ShouldSatisfyAllConditions(errorsList =>
+        {
+            errorsList.Count().ShouldBe(1);
+            errorsList.ShouldContain(error => error.GetString()!.Equals(expectedErrorMessage));
+        });
+    }
+
+    private static MultipartFormDataContent BuildFormData(Stream file, string fieldName)
+    {
+        var formData = new MultipartFormDataContent();
+
+        var fileContent = new StreamContent(file);
+        fileContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+        formData.Add(fileContent, fieldName, "file");
+
+        return formData;
+    }
+}
