a8b06a58086e294f67cef7fc0d052bc56df1470c
Mikael Santos <mykaelsantos@outlook.com.br>
2026-08-24 13:56:56 -0300

    refactoring to return the user image

--- src/Shared/MyRecipeBook.Communication/Responses/User/ResponseUserProfileJson.cs
+++ src/Shared/MyRecipeBook.Communication/Responses/User/ResponseUserProfileJson.cs
@@ -3,5 +3,6 @@
 public class ResponseUserProfileJson
 {
     public string Name { get; set; } = string.Empty;
+    public string ImagemUrl { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
 }
