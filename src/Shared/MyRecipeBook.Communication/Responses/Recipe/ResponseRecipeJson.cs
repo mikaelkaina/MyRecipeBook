1fca5e3a56aafeebcee76d521c56254aa4059e52
Mikael Santos <mykaelsantos@outlook.com.br>
2026-08-25 10:51:57 -0300

    refactoring name properties

--- src/Shared/MyRecipeBook.Communication/Responses/Recipe/ResponseRecipeJson.cs
+++ src/Shared/MyRecipeBook.Communication/Responses/Recipe/ResponseRecipeJson.cs
@@ -6,6 +6,7 @@ public class ResponseRecipeJson
 {
     public Guid Id { get; set; }
     public string Title { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
     public CookTime CookTime { get; set; }
     public IList<string> Ingredients { get; set; } = [];
     public ICollection<ResponseRecipeInstructionJson> Instructions { get; set; } = [];
